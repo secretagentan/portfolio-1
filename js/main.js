9485803d3c0605000c01124efac671f18b3d7706
Abi Munoz <abimunozr@gmail.com>
2017-04-03 03:20:28 -0700

    adds interact.js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// INTERACT.JS
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    // enable autoScroll
+    autoScroll: true,
+
+    // call this function on every dragmove evt
+    onmove: dragMoveListener,
+    // call this function on every dragend evt
+    onend: function (evt) {
+        Math.sqrt(evt.dx * evt.dx + evt.dy * evt.dy)|0;
+    }
+  });
+
+  function dragMoveListener (evt) {
+    var target = evt.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + evt.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + evt.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
